d6d4a54a9e356e5cc79b66f7adccf5b30b8e6d9b
tsuki <lucasmoon0@gmail.com>
2022-09-22 16:16:52 +0200

    fixed broken image url bug

--- frontend/src/components/Editor.js
+++ frontend/src/components/Editor.js
@@ -52,7 +52,7 @@ class Editor extends React.Component {
       const item = {
         title: this.props.title,
         description: this.props.description,
-        image: this.props.image || "http://localhost:3001/placeholder.png",
+        image: this.props.image,
         tagList: this.props.tagList,
       };
 
